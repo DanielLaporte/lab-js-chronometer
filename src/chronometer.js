c4f7bd46c8bfc5cf759a844ee94c744021925be4
DanielLaporte <daniel.laporte2008@gmail.com>
2023-09-27 21:02:44 +0200

    terminado

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,31 +1,47 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    if (!this.intervalId) {
+      this.intervalId = setInterval(() => {
+        this.currentTime += 1;
+        if (printTimeCallback) {
+          printTimeCallback();
+        }
+      }, 1000);
+    }
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      return `0${value}`;
+    } else {
+      return `${value}`;
+    }
   }
 
   stop() {
-    // ... your code goes here
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
+
 
   split() {
     // ... your code goes here
